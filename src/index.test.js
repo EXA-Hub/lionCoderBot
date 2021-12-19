a735c8705685e8ce81f7e129abb789dd84b5d384
EXA-Hub <vip.mimo2015@gmail.com>
2021-12-19 21:37:01 +0200

    edit TEST.js

--- src/index.test.js
+++
@@ -1,8 +0,0 @@
-const { Database } = require("quick.mongodb");
-// mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false
-const db = new Database("mongodb://localhost:27017/quickmongodb");
-
-db.set('d', `dsconst [propertyName] = arrayToDestruct
-
-dsds
-dsds`).then(console.log);
