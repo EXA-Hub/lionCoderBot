d7621254b4811c63c3aad251320930c53ea2cded
achraf7686 <84931869+achraf7686@users.noreply.github.com>
2022-04-12 18:44:35 +0200

    Update config.js

--- src/data/config.js
+++ src/data/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    token: process.env.DISCORD_BOT_TOKEN || '',
-    prefix: '.'
+    token: process.env.DISCORD_BOT_TOKEN || 'ODQ2Nzk5ODAwMzA5NjQ1MzQ0.YK0xxQ.nOb8HLwasu3Hb-_keCBusnXD6s8',
+    prefix: '+'
 };
